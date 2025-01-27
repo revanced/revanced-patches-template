b21aafee74294c4407c31bfac8ec529dae029794
Mubelotix <mubelotix@gmail.com>
2025-01-28 00:28:22 +0100

    Change url rewrite

--- extensions/extension/src/main/java/app/revanced/extension/LoggerPatch.java
+++ extensions/extension/src/main/java/app/revanced/extension/LoggerPatch.java
@@ -12,21 +12,29 @@ public class LoggerPatch {
     public static Request printAndModifyUrl(Request request) {
         // Log the original request
         Log.d("LoggerPatch", "Original request: " + request.toString());
-
-        // Modify the URL
+    
+        // Extract the original URL
         HttpUrl originalUrl = request.url();
-        HttpUrl modifiedUrl = originalUrl.newBuilder()
-                .host(originalUrl.host() + ".reddit-proxy.mub.lol")
+        
+        // Extract the host and path from the original URL
+        String originalHost = originalUrl.host();
+        String originalPath = originalUrl.encodedPath();
+    
+        // Construct the new URL
+        HttpUrl modifiedUrl = new HttpUrl.Builder()
+                .scheme("https")
+                .host("proxy.lol")
+                .encodedPath("/" + originalHost + originalPath)
                 .build();
-
+    
         // Create a new Request with the modified URL
         Request modifiedRequest = request.newBuilder()
                 .url(modifiedUrl)
                 .build();
-
+    
         // Log the modified request
         Log.d("LoggerPatch", "Modified request: " + modifiedRequest.toString());
-
+    
         // Return the modified Request
         return modifiedRequest;
     }
